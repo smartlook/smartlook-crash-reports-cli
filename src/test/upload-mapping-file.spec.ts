8047c3838d8cdfdebf57fe91ec3bade2e04390d0
martinrucki <marthy94@gmail.com>
2021-10-27 19:11:12 +0200

    feat: support force flag

--- src/test/upload-mapping-file.spec.ts
+++ src/test/upload-mapping-file.spec.ts
@@ -1,22 +1,32 @@
-import {uploadMappingFile} from "../lib/commands/upload-mapping-file";
+import {uploadMappingFile, uploadTo} from "../lib/commands/upload-mapping-file";
 import nock from "nock";
 
 describe('uploadMappingFile', () => {
     const token = 'api-token'
     const path = `${__dirname}/test-mapping-file.txt`
     const appVersion = '0.0.1'
-    const internalAppVersion = 'beta.1234'
+    const internalVersion = 'beta.1234'
+    const force = true
 
-    it('should send mapping file to public-api', async () => {
+    describe('uploadTo', () => {
 
-        nock('https://public-api.alfa.smartlook.cloud:443', {"encodedQueryParams": true})
-            .post('/api/v1/releases/0.0.1/mapping-files', (body) => {
-                expect(body.toString().includes("content of test mapping file")).toEqual(true)
-                return true
+        it('should send mapping file to public-api', async () => {
+
+            nock('https://public-api.alfa.smartlook.cloud:443', {"encodedQueryParams": true})
+                .post('/api/v1/releases/0.0.1/mapping-files', (body) => {
+                    expect(body.toString().includes("content of test mapping file")).toEqual(true)
+                    return true
+                })
+                .query({"force": "true"})
+                .reply(201, {});
+
+            await uploadTo('https://public-api.alfa.smartlook.cloud:443/api/v1/releases/0.0.1/mapping-files', {
+                token,
+                path,
+                appVersion,
+                internalVersion,
+                force
             })
-            .query({"force": "true"})
-            .reply(201, {});
-
-        await uploadMappingFile({token, path, appVersion, internalAppVersion})
+        })
     })
 })
