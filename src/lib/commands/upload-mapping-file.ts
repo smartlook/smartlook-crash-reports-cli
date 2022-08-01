4cfbc57867dd73eb1f80fbd4f1b165b7c1ab2a80
Patrik Laurinc <patrik.laurinc99@gmail.com>
2022-08-01 12:24:28 +0200

    chore: add prettierrc and format

--- src/lib/commands/upload-mapping-file.ts
+++ src/lib/commands/upload-mapping-file.ts
@@ -5,79 +5,85 @@ import FormData from 'form-data'
 const HOST = 'https://api.smartlook.cloud'
 
 interface CLIArgs {
-    path: string;
-    token: string;
-    appVersion: string;
-    platform: 'android' | 'ios';
-    bundleId: string;
-    internalAppVersion?: string;
-    force?: boolean;
+	path: string
+	token: string
+	appVersion: string
+	platform: 'android' | 'ios'
+	bundleId: string
+	internalAppVersion?: string
+	force?: boolean
 }
 
 function validateInput(args: CLIArgs) {
-    if (!args.path) {
-        throw new Error("Missing path")
-    }
-    if (!args.token) {
-        throw new Error("Missing token")
-    }
-    if (!args.appVersion) {
-        throw new Error("Missing App version")
-    }
-    if (!args.platform) {
-        throw new Error("Missing platform")
-    }
-    if (!args.bundleId) {
-        throw new Error("Missing bundleId")
-    }
+	if (!args.path) {
+		throw new Error('Missing path')
+	}
+	if (!args.token) {
+		throw new Error('Missing token')
+	}
+	if (!args.appVersion) {
+		throw new Error('Missing App version')
+	}
+	if (!args.platform) {
+		throw new Error('Missing platform')
+	}
+	if (!args.bundleId) {
+		throw new Error('Missing bundleId')
+	}
 }
 
 function makeOptions(args: CLIArgs) {
-    const searchParams = {'force': args.force == true ? 'true' : 'false'}
+	const searchParams = { force: args.force == true ? 'true' : 'false' }
 
-    const form = new FormData()
-    const readMappingFile = fs.createReadStream(args.path)
-    form.append('mappingFile', readMappingFile)
+	const form = new FormData()
+	const readMappingFile = fs.createReadStream(args.path)
+	form.append('mappingFile', readMappingFile)
 
-    if (args.internalAppVersion) {
-        form.append('internalAppVersion', args.internalAppVersion)
-    }
+	if (args.internalAppVersion) {
+		form.append('internalAppVersion', args.internalAppVersion)
+	}
 
-    const headers = {'Authorization': `Bearer ${args.token}`, ...form.getHeaders()}
+	const headers = {
+		Authorization: `Bearer ${args.token}`,
+		...form.getHeaders(),
+	}
 
-    return {
-        searchParams,
-        headers,
-        body: form,
-    }
+	return {
+		searchParams,
+		headers,
+		body: form,
+	}
 }
 
 async function uploadTo(destinationUrl: string, args: CLIArgs): Promise<void> {
-    const requestOptions = makeOptions(args)
+	const requestOptions = makeOptions(args)
 
-    console.log("Started uploading mapping file")
-    try {
-      const { body, statusCode } = await got.post(destinationUrl, requestOptions)
+	console.log('Started uploading mapping file')
+	try {
+		const { body, statusCode } = await got.post(destinationUrl, requestOptions)
 
-      if (statusCode !== 201) {
-        throw new Error(`Uploading mapping file failed with status code ${statusCode}`);
-      }
+		if (statusCode !== 201) {
+			throw new Error(
+				`Uploading mapping file failed with status code ${statusCode}`
+			)
+		}
 
-      console.log('Uplading successfull: ', body)
-    } catch (e) {
-      console.error(e)
-    }
+		console.log('Uplading successfull: ', body)
+	} catch (e) {
+		console.error(e)
+	}
 }
 
 export async function uploadMappingFile(args: CLIArgs): Promise<void> {
-    try {
-        validateInput(args)
-    } catch (e) {
-        console.log(e)
-        return
-    }
+	try {
+		validateInput(args)
+	} catch (e) {
+		console.log(e)
+		return
+	}
 
-    const publicApiUrl = `${HOST}/api/v1/bundles/${args.bundleId}/platforms/${args.platform}/releases/${args.appVersion}/mapping-files`
+	const publicApiUrl = `${HOST}/api/v1/bundles/${args.bundleId}/platforms/${args.platform}/releases/${args.appVersion}/mapping-files`
 
-    await uploadTo(publicApiUrl, args)
+	await uploadTo(publicApiUrl, args)
 }
+
--- src/test/upload-mapping-file.spec.ts
+++ src/test/upload-mapping-file.spec.ts
@@ -1,40 +1,47 @@
-import { uploadMappingFile } from "../lib/commands/upload-mapping-file";
-import nock from "nock";
+import { uploadMappingFile } from '../lib/commands/upload-mapping-file'
+import nock from 'nock'
 
 describe('uploadMappingFile', () => {
-    const token = 'api-token'
-    const path = `${__dirname}/test-mapping-file.txt`
-    const bundleId = 'prod'
-    const appVersion = '0.0.1'
-    const internalAppVersion = 'beta.1234'
-    const force = true
+	const token = 'api-token'
+	const path = `${__dirname}/test-mapping-file.txt`
+	const bundleId = 'prod'
+	const appVersion = '0.0.1'
+	const internalAppVersion = 'beta.1234'
+	const force = true
 
-    describe('uploadTo', () => {
+	describe('uploadTo', () => {
+		it('should send mapping file to public-api', async () => {
+			const uploadingNock = nock('https://api.smartlook.cloud', {
+				encodedQueryParams: true,
+			})
+				.post(
+					'/api/v1/bundles/prod/platforms/android/releases/0.0.1/mapping-files',
+					(body) => {
+						expect(
+							body.toString().includes('content of test mapping file')
+						).toEqual(true)
+						return true
+					}
+				)
+				.query({ force: 'true' })
+				.reply(201, {
+					oid: 'oid',
+					createdAt: new Date('2021-09-10'),
+					state: 'completed',
+				})
 
-        it('should send mapping file to public-api', async () => {
-            const uploadingNock = nock('https://api.smartlook.cloud', {"encodedQueryParams": true})
-                .post('/api/v1/bundles/prod/platforms/android/releases/0.0.1/mapping-files', (body) => {
-                    expect(body.toString().includes("content of test mapping file")).toEqual(true)
-                    return true
-                })
-                .query({"force": "true"})
-                .reply(201, {
-                  oid: 'oid',
-                  createdAt: new Date('2021-09-10'),
-                  state: 'completed',
-                });
+			await uploadMappingFile({
+				token,
+				path,
+				bundleId,
+				appVersion,
+				platform: 'android',
+				internalAppVersion,
+				force,
+			})
 
-            await uploadMappingFile({
-                token,
-                path,
-                bundleId,
-                appVersion,
-                platform: 'android',
-                internalAppVersion,
-                force
-            })
-
-            expect(uploadingNock.isDone()).toBeTruthy()
-        })
-    })
+			expect(uploadingNock.isDone()).toBeTruthy()
+		})
+	})
 })
+
