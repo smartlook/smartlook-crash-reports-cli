51021d4fb0710249c64f74140d08ca57f91530a6
Patrik Laurinc <patrik.laurinc99@gmail.com>
2022-08-01 12:22:41 +0200

    chore: add prettierrc and fix format

--- src/lib/commands/upload-mapping-file.ts
+++ src/lib/commands/upload-mapping-file.ts
@@ -1,143 +1,143 @@
-import * as fs from "fs";
-import got from "got";
-import FormData from "form-data";
-import archiver from "archiver";
+import * as fs from 'fs'
+import got from 'got'
+import FormData from 'form-data'
+import archiver from 'archiver'
 
-const HOST = "https://api.smartlook.cloud";
+const HOST = 'https://api.smartlook.cloud'
 
 interface CLIArgs {
-  path: string;
-  token: string;
-  appVersion: string;
-  platform: "android" | "ios";
-  bundleId: string;
-  internalAppVersion?: string;
-  force?: boolean;
+	path: string
+	token: string
+	appVersion: string
+	platform: 'android' | 'ios'
+	bundleId: string
+	internalAppVersion?: string
+	force?: boolean
 }
 
 interface RequestOptions {
-  searchParams: Record<string, string>;
-  body: FormData;
-  headers: Record<string, string>;
+	searchParams: Record<string, string>
+	body: FormData
+	headers: Record<string, string>
 }
 
 function validateInput(args: CLIArgs) {
-  if (!args.path) {
-    throw new Error("Missing path");
-  }
-  if (!args.token) {
-    throw new Error("Missing token");
-  }
-  if (!args.appVersion) {
-    throw new Error("Missing App version");
-  }
-  if (!args.platform) {
-    throw new Error("Missing platform");
-  }
-  if (!args.bundleId) {
-    throw new Error("Missing bundleId");
-  }
+	if (!args.path) {
+		throw new Error('Missing path')
+	}
+	if (!args.token) {
+		throw new Error('Missing token')
+	}
+	if (!args.appVersion) {
+		throw new Error('Missing App version')
+	}
+	if (!args.platform) {
+		throw new Error('Missing platform')
+	}
+	if (!args.bundleId) {
+		throw new Error('Missing bundleId')
+	}
 }
 
 function makeOptions(
-  args: CLIArgs,
-  zipFile?: archiver.Archiver
+	args: CLIArgs,
+	zipFile?: archiver.Archiver
 ): RequestOptions {
-  const searchParams = { force: args.force == true ? "true" : "false" };
-  const form = new FormData();
+	const searchParams = { force: args.force == true ? 'true' : 'false' }
+	const form = new FormData()
 
-  const readMappingFile = zipFile || fs.createReadStream(args.path);
+	const readMappingFile = zipFile || fs.createReadStream(args.path)
 
-  form.append(
-    "mappingFile",
-    readMappingFile,
-    zipFile ? { knownLength: zipFile.pointer() } : undefined
-  );
+	form.append(
+		'mappingFile',
+		readMappingFile,
+		zipFile ? { knownLength: zipFile.pointer() } : undefined
+	)
 
-  if (args.internalAppVersion) {
-    form.append("internalAppVersion", args.internalAppVersion);
-  }
+	if (args.internalAppVersion) {
+		form.append('internalAppVersion', args.internalAppVersion)
+	}
 
-  const headers = {
-    Authorization: `Bearer ${args.token}`,
-    ...form.getHeaders(),
-  };
+	const headers = {
+		Authorization: `Bearer ${args.token}`,
+		...form.getHeaders(),
+	}
 
-  return {
-    searchParams,
-    headers,
-    body: form,
-  };
+	return {
+		searchParams,
+		headers,
+		body: form,
+	}
 }
 async function uploadAndroid(
-  destinationUrl: string,
-  args: CLIArgs
+	destinationUrl: string,
+	args: CLIArgs
 ): Promise<void> {
-  const requestOptions = makeOptions(args);
-  await upload(destinationUrl, requestOptions);
+	const requestOptions = makeOptions(args)
+	await upload(destinationUrl, requestOptions)
 }
 
 async function uploadIos(destinationUrl: string, args: CLIArgs): Promise<void> {
-  const archive = archiver("zip", {
-    zlib: { level: 6 },
-  });
+	const archive = archiver('zip', {
+		zlib: { level: 6 },
+	})
 
-  archive.glob("**/DWARF/*", {
-    cwd: args.path,
-  });
+	archive.glob('**/DWARF/*', {
+		cwd: args.path,
+	})
 
-  await new Promise((resolve, reject) => {
-    archive.on("finish", async () => {
-      const requestOptions = makeOptions(args, archive);
+	await new Promise((resolve, reject) => {
+		archive.on('finish', async () => {
+			const requestOptions = makeOptions(args, archive)
 
-      try {
-        await upload(destinationUrl, requestOptions);
-        resolve(true);
-      } catch (e) {
-        console.log(e);
-        reject();
-      }
-    });
+			try {
+				await upload(destinationUrl, requestOptions)
+				resolve(true)
+			} catch (e) {
+				console.log(e)
+				reject()
+			}
+		})
 
-    archive.on("error", (err) => reject);
+		archive.on('error', (err) => reject)
 
-    archive.finalize();
-  });
+		archive.finalize()
+	})
 }
 
 async function upload(
-  destinationUrl: string,
-  requestOptions: RequestOptions
+	destinationUrl: string,
+	requestOptions: RequestOptions
 ): Promise<void> {
-  console.log("Started uploading mapping file");
-  try {
-    const { body, statusCode } = await got.post(destinationUrl, requestOptions);
+	console.log('Started uploading mapping file')
+	try {
+		const { body, statusCode } = await got.post(destinationUrl, requestOptions)
 
-    if (statusCode !== 201) {
-      throw new Error(
-        `Uploading mapping file failed with status code ${statusCode}`
-      );
-    }
+		if (statusCode !== 201) {
+			throw new Error(
+				`Uploading mapping file failed with status code ${statusCode}`
+			)
+		}
 
-    console.log("Uplading successfull: ", body);
-  } catch (e) {
-    throw new Error("Uploading failed");
-  }
+		console.log('Uplading successfull: ', body)
+	} catch (e) {
+		throw new Error('Uploading failed')
+	}
 }
 
 export async function uploadMappingFile(args: CLIArgs): Promise<void> {
-  try {
-    validateInput(args);
-  } catch (e) {
-    console.log(e);
-    return;
-  }
+	try {
+		validateInput(args)
+	} catch (e) {
+		console.log(e)
+		return
+	}
 
-  const publicApiUrl = `${HOST}/api/v1/bundles/${args.bundleId}/platforms/${args.platform}/releases/${args.appVersion}/mapping-files`;
+	const publicApiUrl = `${HOST}/api/v1/bundles/${args.bundleId}/platforms/${args.platform}/releases/${args.appVersion}/mapping-files`
 
-  if (args.platform === "android") {
-    await uploadAndroid(publicApiUrl, args);
-  } else {
-    await uploadIos(publicApiUrl, args);
-  }
+	if (args.platform === 'android') {
+		await uploadAndroid(publicApiUrl, args)
+	} else {
+		await uploadIos(publicApiUrl, args)
+	}
 }
--- src/test/upload-mapping-file.spec.ts
+++ src/test/upload-mapping-file.spec.ts
@@ -1,90 +1,90 @@
-import { uploadMappingFile } from "../lib/commands/upload-mapping-file";
-import nock from "nock";
+import { uploadMappingFile } from '../lib/commands/upload-mapping-file'
+import nock from 'nock'
 
 const hexToString = (hex: any) => {
-  let str = "";
-  for (let i = 0; i < hex.length; i += 2)
-    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-  return str;
-};
+	let str = ''
+	for (let i = 0; i < hex.length; i += 2)
+		str += String.fromCharCode(parseInt(hex.substr(i, 2), 16))
+	return str
+}
 
-describe("uploadMappingFile", () => {
-  const token = "api-token";
-  const path = `${__dirname}/test-mapping-file.txt`;
-  const dsymPath = `${__dirname}/test.xcarchive`;
-  const bundleId = "prod";
-  const appVersion = "0.0.1";
-  const internalAppVersion = "beta.1234";
-  const force = true;
+describe('uploadMappingFile', () => {
+	const token = 'api-token'
+	const path = `${__dirname}/test-mapping-file.txt`
+	const dsymPath = `${__dirname}/test.xcarchive`
+	const bundleId = 'prod'
+	const appVersion = '0.0.1'
+	const internalAppVersion = 'beta.1234'
+	const force = true
 
-  describe("uploadTo", () => {
-    it("should send mapping file to public-api", async () => {
-      const uploadingNock = nock("https://api.smartlook.cloud", {
-        encodedQueryParams: true,
-      })
-        .post(
-          "/api/v1/bundles/prod/platforms/android/releases/0.0.1/mapping-files",
-          (body) => {
-            expect(
-              body.toString().includes("content of test mapping file")
-            ).toEqual(true);
-            return true;
-          }
-        )
-        .query({ force: "true" })
-        .reply(201, {
-          oid: "oid",
-          createdAt: new Date("2021-09-10"),
-          state: "completed",
-        });
+	describe('uploadTo', () => {
+		it('should send mapping file to public-api', async () => {
+			const uploadingNock = nock('https://api.smartlook.cloud', {
+				encodedQueryParams: true,
+			})
+				.post(
+					'/api/v1/bundles/prod/platforms/android/releases/0.0.1/mapping-files',
+					(body) => {
+						expect(
+							body.toString().includes('content of test mapping file')
+						).toEqual(true)
+						return true
+					}
+				)
+				.query({ force: 'true' })
+				.reply(201, {
+					oid: 'oid',
+					createdAt: new Date('2021-09-10'),
+					state: 'completed',
+				})
 
-      await uploadMappingFile({
-        token,
-        path,
-        bundleId,
-        appVersion,
-        platform: "android",
-        internalAppVersion,
-        force,
-      });
+			await uploadMappingFile({
+				token,
+				path,
+				bundleId,
+				appVersion,
+				platform: 'android',
+				internalAppVersion,
+				force,
+			})
 
-      expect(uploadingNock.isDone()).toBeTruthy();
-    });
+			expect(uploadingNock.isDone()).toBeTruthy()
+		})
 
-    it("should send mapping file to public-api", async () => {
-      const uploadingNock = nock("https://api.smartlook.cloud", {
-        encodedQueryParams: true,
-      })
-        .post(
-          "/api/v1/bundles/prod/platforms/ios/releases/0.0.1/mapping-files",
-          (body) => {
-            expect(
-              // Multipart will be hex encoded by default, need to decode it
-              hexToString(body).includes(
-                'Content-Disposition: form-data; name="mappingFile"'
-              )
-            ).toBeTruthy();
-            return true;
-          }
-        )
-        .query({ force: "true" })
-        .reply(201, {
-          oid: "oid",
-          createdAt: new Date("2021-09-10"),
-          state: "completed",
-        });
+		it('should send mapping file to public-api', async () => {
+			const uploadingNock = nock('https://api.smartlook.cloud', {
+				encodedQueryParams: true,
+			})
+				.post(
+					'/api/v1/bundles/prod/platforms/ios/releases/0.0.1/mapping-files',
+					(body) => {
+						expect(
+							// Multipart will be hex encoded by default, need to decode it
+							hexToString(body).includes(
+								'Content-Disposition: form-data; name="mappingFile"'
+							)
+						).toBeTruthy()
+						return true
+					}
+				)
+				.query({ force: 'true' })
+				.reply(201, {
+					oid: 'oid',
+					createdAt: new Date('2021-09-10'),
+					state: 'completed',
+				})
 
-      await uploadMappingFile({
-        token,
-        path: dsymPath,
-        bundleId,
-        appVersion,
-        platform: "ios",
-        internalAppVersion,
-        force,
-      });
+			await uploadMappingFile({
+				token,
+				path: dsymPath,
+				bundleId,
+				appVersion,
+				platform: 'ios',
+				internalAppVersion,
+				force,
+			})
 
-      expect(uploadingNock.isDone()).toBeTruthy();
-    });
-  });
-});
+			expect(uploadingNock.isDone()).toBeTruthy()
+		})
+	})
+})
